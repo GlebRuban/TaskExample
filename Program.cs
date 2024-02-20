5da10c6ea526f11e94935d2331232c0567eb4d1c
GlebRuban <104936944+GlebRuban@users.noreply.github.com>
2024-02-21 00:17:09 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+class Program
+{
+    static async Task Main()
+    {
+        Console.WriteLine($"Текущее время: {DateTime.Now}");
+
+        var task1 = new Task(() =>
+        {
+            Console.WriteLine($"Задача запущена через метод Start: {DateTime.Now}");
+        });
+        task1.Start();
+
+        var task2 = Task.Factory.StartNew(() =>
+        {
+            Console.WriteLine($"Задача запущена через Task.Factory.StartNew: {DateTime.Now}");
+        });
+
+        var task3 = Task.Run(() =>
+        {
+            Console.WriteLine($"Задача запущена через Task.Run: {DateTime.Now}");
+        });
+
+        await Task.WhenAll(task1, task2, task3);
+    }
+}
---
+++ Task2and3.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskExample
+{
+    class Task2and3
+    {
+        static void Main()
+        {
+            // Задание 2
+            Task task1 = Task.Run(() =>
+            {
+                Console.WriteLine("Простые числа от 0 до 1000:");
+                for (int i = 2; i <= 1000; i++)
+                {
+                    if (IsPrime(i))
+                    {
+                        Console.Write(i + " ");
+                    }
+                }
+            });
+
+            task1.Wait(); 
+            // Задание 3
+            int lowerBound = 1000;
+            int upperBound = 2000;
+
+            Task<int> task2 = Task.Run(() =>
+            {
+                int count = 0;
+                for (int i = lowerBound; i <= upperBound; i++)
+                {
+                    if (IsPrime(i))
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            });
+
+            int primeCount = task2.Result;
+
+            Console.WriteLine($"\nКоличество простых чисел от {lowerBound} до {upperBound}: {primeCount}");
+        }
+        static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            for (int i = 3; i <= Math.Sqrt(number); i += 2)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
